252104fe745b66c09626f12d72a55f46770e3aec
tazzaenciel <tazzaenciel@gmail.com>
2020-07-07 17:01:13 +0900

    후원도

--- util/binary_tree_mem.php
+++ util/binary_tree_mem.php
@@ -4,7 +4,8 @@ include_once('./_common.php');
 header('Content-Type: application/json');
 
 // $sql = "select * from g5_member as m where mb_recommend='{$_GET['mb_id']}' and mb_level >= 2 and mb_brecommend='' order by mb_datetime desc";
-$sql = "select * from g5_member as m where mb_recommend='{$_POST['mb_id']}' and grade > 0 and mb_brecommend='' order by mb_datetime desc";
+// $sql = "select * from g5_member as m where mb_recommend='{$_POST['mb_id']}' and grade > 0 and mb_brecommend='' order by mb_datetime desc";
+$sql = "select * from g5_member as m where mb_recommend='{$_POST['mb_id']}' and mb_brecommend='' order by mb_datetime desc";
 
 $sth = sql_query($sql);
 
