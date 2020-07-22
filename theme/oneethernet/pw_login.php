0c702c16bb24e8ed90a1d0691ac6d5cebe47fa32
tazzaenciel <tazzaenciel@gmail.com>
2020-07-22 10:17:55 +0900

    회원가입 가능

--- theme/oneethernet/pw_login.php
+++ theme/oneethernet/pw_login.php
@@ -35,8 +35,8 @@
 
 				<div class="login_btn_bottom">
 					<button type="button" class="btn btn_wd btn_primary" onclick="flogin_submit();" ><span>LOG IN</span></button>
-					<!-- <a href="/bbs/register_form.php" class="btn btn_wd btn_default"><span data-i18n="login.신규 회원 등록하기">Create new account</span></a> -->
-					<a href="javascript:temp_block();" class="btn btn_wd btn_default"><span data-i18n="login.신규 회원 등록하기">Create new account</span></a>
+					<a href="/bbs/register_form.php" class="btn btn_wd btn_default"><span data-i18n="login.신규 회원 등록하기">Create new account</span></a>
+					<!-- <a href="javascript:temp_block();" class="btn btn_wd btn_default"><span data-i18n="login.신규 회원 등록하기">Create new account</span></a> -->
 					<!--<a href="javascript:serviceModal();"class='desc'>lost password?</a>-->
 						<!--<a href="<?=G5_BBS_URL?>/password_lost.php" class='desc'>lost password?</a>-->
 						<a href="mailto:hazinformatioin@gmail.com" onclick='showhelp();' class='desc'>Need you Help?</a>
