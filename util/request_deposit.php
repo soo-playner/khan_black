f52b03f2617f4263dbc34df735c32350f9277a34
root <root@localhost.localdomain>
2020-07-25 13:25:40 +0900

    입금요청 재수정

--- util/request_deposit.php
+++ util/request_deposit.php
@@ -10,7 +10,7 @@ $mb_id = $_POST['mb_id'];
 $txhash = $_POST['hash'];
 $coin = $_POST['coin'];
 
-$sql = "INSERT INTO wallet_deposit_request(mb_id, txhash, create_dt,create_d,status,coin) VALUES('$mb_id','$txhash','$now_datetime','$now_date',1,'$coin')";
+$sql = "INSERT INTO wallet_deposit_request(mb_id, txhash, create_dt,create_d,status,coin) VALUES('$mb_id','$txhash','$now_datetime','$now_date',0,'$coin')";
 $result = sql_query($sql);
 
 if($result){
