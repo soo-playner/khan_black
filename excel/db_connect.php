3d486aae2bc50e40009ae1e4307bb76519fad74c
tazzaenciel <tazzaenciel@gmail.com>
2020-07-29 17:34:31 +0900

    db비밀번호

--- excel/db_connect.php
+++ excel/db_connect.php
@@ -1,8 +1,7 @@
 <?php
 $host_name = 'localhost';
 $user_name = 'root';
-// $user_pwd = 'htg3dnjf25dlf!@#$5';
-$user_pwd = 'willsoft0780!@';
+$user_pwd = 'htg3dnjf25dlf!@#$5';
 $database = 'oneethernet';
 $conn = mysqli_connect($host_name,$user_name,$user_pwd,$database);
  ?>
